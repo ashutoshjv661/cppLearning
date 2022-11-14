45a61a1fe79394590dd67dc4e806c8f3d1d464da
Ashutosh Jadhav <ashutoshjadhav661@gmail.com>
2022-11-14 17:57:28 +0530

    added enum code cleanup

--- logger/logger.cpp
+++ logger/logger.cpp
@@ -4,36 +4,36 @@
 class Log
 {
     public:
-        const int LogLevelError = 0;
-        const int LogLevelWarning = 1;
-        const int LogLevelInfo= 2; //for trace
+        enum Level{
+            LevelError = 0,LevelWarning,LevelInfo
+        };
     private: 
-        int m_LogLevel = LogLevelInfo; //by default 
+        Level m_LogLevel = LevelInfo; //by default 
     public:
-        void SetLevel(int level)
+        void SetLevel(Level level)
         {
             m_LogLevel = level;
         }
         void Error(const char* message)
         {
-            if(m_LogLevel>=LogLevelError)
+            if(m_LogLevel>=LevelError)
             std::cout<<"[ERROR] => "<<message<<std::endl;
         }
         void Warn(const char* message)
         {
-            if(m_LogLevel>=LogLevelWarning)
+            if(m_LogLevel>=LevelWarning)
             std::cout<<"[WARNING] => "<<message<<std::endl;
         }
         void Info(const char* message)
         {
-            if(m_LogLevel>=LogLevelInfo)
+            if(m_LogLevel>=LevelInfo)
             std::cout<<"[INFO] => "<<message<<std::endl;
         }
 };
 int main()
 {
     Log log;
-    log.SetLevel(log.LogLevelInfo);
+    log.SetLevel(Log::LevelInfo);
     log.Warn("Hello!");
     log.Info("Hello!");
     log.Error("Hello!");
